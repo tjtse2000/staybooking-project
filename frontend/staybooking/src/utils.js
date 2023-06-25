72f7d072ae682b4ee3e267cd3e80ea8e02c96888
TINGJUN XIE <tx668@nyu.edu>
2023-06-25 11:46:36 -0400

    Update utils.js
    Reset the URL to localhost

--- frontend/staybooking/src/utils.js
+++ frontend/staybooking/src/utils.js
@@ -1,4 +1,4 @@
-const domain = "https://boxwood-yen-382002.ue.r.appspot.com";
+const domain = "http://localhost:8080";
 
 export const login = (credential, asHost) => {
     const loginUrl = `${domain}/authenticate/${asHost ? "host" : "guest"}`;
